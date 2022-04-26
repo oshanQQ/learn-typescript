69a50b1020c523473848c53c363ab8c1d65fc936
oshanQQ <oshanqq@gmail.com>
2022-04-26 22:18:41 +0900

    ✨ Complete node task

---
+++ progate-path/node_tutorial/.progate/jest-reporter.js
@@ -0,0 +1,30 @@
+
+export default class JestReporter {
+  constructor(globalConfig, options) {
+    this._testResults = new Map();
+    console.log();
+  }
+
+  onTestCaseResult(t, result) {
+    const extractJudgeId = (title) => {
+      const matches = title.match(/\[(.*)\]$/);
+      if (matches === null) return null;
+      return matches[1];
+    };
+    const judgeId = extractJudgeId(result.title);
+    if (judgeId === null) {
+      throw new Error("No ID in the test's title: '" + result.title + "'");
+    }
+    const hasPassed = result.status === "passed";
+    this._testResults.set(judgeId, hasPassed);
+  }
+
+  async onRunComplete() {
+    console.log("Progate Jest Reporter submits the results to the server...");
+    for (const [judgeId, hasPassed] of this._testResults.entries()) {
+      console.log(judgeId, ":", hasPassed);
+    }
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log("Done!");
+  }
+}
