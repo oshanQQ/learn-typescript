71f539a5c42786f612768529809f1cce5cc22448
oshanQQ <oshanqq@gmail.com>
2022-05-25 11:45:56 +0900

    :sparkles: Create audio record panel

---
+++ csv-recorder/components/AudioRecordPanel.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Box, Button, HStack, Text } from "@chakra-ui/react";
+
+type Props = {
+  sentence: string;
+};
+
+const AudioRecordingPanel = ({ sentence }: Props) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <HStack direction="row" spacing={4}>
+      <Button
+        colorScheme="red"
+        width={24}
+        variant={isClicked ? "solid" : "ghost"}
+        disabled={isClicked}
+        onClick={() => setIsClicked(!isClicked)}
+      >
+        {isClicked ? "録音中" : "録音する"}
+      </Button>
+      <Button
+        colorScheme="gray"
+        variant="ghost"
+        disabled={!isClicked}
+        onClick={() => setIsClicked(false)}
+      >
+        停止
+      </Button>
+      <Text>{sentence}</Text>
+    </HStack>
+  );
+};
+
+export default AudioRecordingPanel;
